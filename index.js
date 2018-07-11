30bfd581ce0deaea03f634f60f2df3ef86dc6218
utkarsh_bajpai <ubajpai1aa@gmail.com>
2018-07-11 18:44:11 +0530

    basic set-up of the web sockets
    web sockets successfully installed on the client and the server side

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var socket = require('socket.io');
 
 var app = express();
 
 var server = app.listen(4000, function(){
 	console.log('server is running on port 4000');
+});
+
+var io = socket(server);
+io.on('connection', function(socket){
+	console.log(' connection made using web sockets ' + socket.id);
 });
 
 app.use(express.static('public'));
